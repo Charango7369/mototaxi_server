0fbeffa574806f208d94c9337c1d8c250c9a535f
Edwin Rivero <edwinjrivero@gmail.com>
2026-04-07 17:52:12 -0400

    fix: eliminar WSUpgradeMiddleware que rompia WebSocket en Railway

--- app/main.py
+++ app/main.py
@@ -18,20 +18,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Middleware para forzar upgrade WebSocket en Railway
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.requests import Request
-
-class WSUpgradeMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        if request.url.path.startswith("/ws"):
-            # Asegurar headers correctos para WebSocket
-            headers = dict(request.headers)
-            headers["upgrade"] = "websocket"
-        return await call_next(request)
-
-app.add_middleware(WSUpgradeMiddleware)
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
@@ -43,6 +29,7 @@ app.include_router(stats_router.router)
 app.include_router(monitor_router.router)
 
 Base.metadata.create_all(bind=engine)
+
 
 @app.get("/", response_class=FileResponse)
 def root():
