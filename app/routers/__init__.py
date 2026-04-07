e031b6fa2293430abf73941de7e6d4a391bc57a1
Edwin Rivero <edwinjrivero@gmail.com>
2026-04-07 17:50:44 -0400

    fix: vaciar __init__.py para eliminar import circular

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,3 +1,1 @@
-from app.routers import driver_router, ride_router, ws_router, stats_router, monitor_router
 
-__all__ = ["driver_router", "ride_router", "ws_router", "stats_router", "monitor_router"]
